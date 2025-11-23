289633b6c930e6fa54b4f378de3ad2a9b4ffc6ba
holbiejlln <11078@holbertonstudents.com>
2025-11-23 16:42:10 +0000

    t0

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,4 +1,25 @@
 #!/usr/bin/python3
+"""
+Module: 0-square
+
+This module defines the Square class, which represents a square
+with a private size attribute. It allows instantiation of
+Square objects with a specific size.
+"""
+
 class Square:
-    def __init__(self, size):       
+    """
+    Class that defines a square by its size.
+
+    Attributes:
+        __size (int): The private size of the square.
+    """
+
+    def __init__(self, size):
+        """
+        Initialize a new Square instance.
+
+        Args:
+            size (int): The size of the square.
+        """
         self.__size = size
